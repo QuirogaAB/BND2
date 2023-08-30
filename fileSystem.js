f573c632dd005ec249749d7bb3673972043180a3
QuirogaAB <quiroga.ab@gmail.com>
2023-08-29 23:53:48 -0400

    Configurando entorno de entregable 2

---
+++ fileSystem.js
@@ -0,0 +1,32 @@
+import {promises as fs} from "fs"
+
+class ProductManager {
+    constructor(){
+        this.patch = "./productos.txt"
+        
+        this.nextId = 1   
+    }
+
+    addProduct = async (title, description, price, thumbnail, code, stock) => {
+
+        ProductManager.Id++
+
+        let newProduct = {
+            id: ProductManager.Id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        console.log(newProduct)
+
+        //await fs.writeFile(this.patch, "...")
+    }
+}
+
+const productos = new ProductManager
+
+productos.addProduct("Pañales Dia", "Pack de pañales para adulto absorventes de uso diario", 12000, "imagen1", "pa0355", 30)
